bf428761e4a0bc5a48d5c957467d5beed7a7907b
Ashis Kumar Chanda <ashis415@yahoo.com>
2021-08-26 15:35:36 -0400

    Update head.php

--- application/views/includes/head.php
+++ application/views/includes/head.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html lang="en-US">
 <head>
-	<title>CCC | Center for Cognitive Computing | Temple University</title>
+	<title>CHI | Center for Hybrid Intelligence | Temple University</title>
 	<meta charset="utf-8" />
 	<meta name="author" content="iGEM" />
-	<meta name="description" content="Temple University's Center for Cognitive Computing website" />
-	<meta name="keyword" content="Cognitive, Computing, Cognitive Computing, Temple University" />
+	<meta name="description" content="Temple University's Center for Hybrid Intelligence" />
+	<meta name="keyword" content="Hybrid, Intelligence, Hybrid Intelligence, Temple University" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link href="favicon.ico" rel="icon" type="image/x-icon" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
