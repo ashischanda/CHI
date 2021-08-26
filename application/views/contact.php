04d207feadc0c400cc67a5c9c83cdc4aa97a9c7d
Ashis Kumar Chanda <ashis415@yahoo.com>
2021-08-26 15:33:39 -0400

    Update contact.php

--- application/views/contact.php
+++ application/views/contact.php
@@ -1,11 +1,11 @@
 
-		<div class="row">
-			<div class="col-md-3">
+    <div class="row">
+      <div class="col-md-3">
         <div class="site-header">
-				  <h1>Contact</h1>
+          <h1>Contact</h1>
         </div>
-				<address>
-          <strong>Center for Cognitive Computing</strong><br />
+        <address>
+          <strong>Center for Hybrid Intelligence</strong><br />
           Temple University<br />
           1925 N. 12th St., SERC 314<br />
           Philadelphia, PA 19122<br />
@@ -13,8 +13,8 @@
           <strong>Phone:</strong> 215-204-8450<br />
           <strong>Email:</strong> <a href="mailto:vucetic@temple.edu">vucetic@temple.edu</a>
         </address>
-			</div>
-			<div class="col-md-9">
+      </div>
+      <div class="col-md-9">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3057.193196775514!2d-75.1552578841988!3d39.98178768989958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6c80a6aaab3c1%3A0x83e756437aed7a9c!2sTemple+University+Science+Education+and+Research+Center!5e0!3m2!1sen!2sus!4v1548773829802" width="100%" height="650" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
-		</div>
+    </div>
