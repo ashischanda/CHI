1c065ada1d68dc49b9b6407c08b10fcfc45a90e9
Ashis Kumar Chanda <ashis415@yahoo.com>
2021-08-26 15:33:23 -0400

    Update about.php

--- application/views/about.php
+++ application/views/about.php
@@ -7,7 +7,7 @@
       </div>
 			<div class="col-sm-12">
         <img src="images/about.jpg" alt="SERC building, cognitive computing graphic and Philadelphia skyline" style="width: 100%; margin-bottom: 15px" />
-				<p>Insert information about the Center for Cognitive Computing (some ideas include what the center is about, what do you do, who is the head, some history, etc)</p>
+				<p> Center for Hybrid Intelligence (CHI) is housed in SERC building and serves Temple University and Greater Philadelphia community</p>
 			</div>
     </div>
     <div class="about-section row">
@@ -15,7 +15,7 @@
 				<img src="images/cognitive-cloud.png" style="width: 100%;" alt="Cognitive computing wordcloud" />
       </div>
       <div class="col-md-6">
-				<h2>What is Cognitive Computing?</h2>
-				<p>Cognitive Computing is an umbrella term that refers to creating software systems that exhibit ability to learn, adapt, understand context, and work seamlessly with humans and for humans. Core areas that are part of Cognitive Computing include artificial intelligence, machine learning, human-computer interaction, natural language processing, computer vision, robotics, information retrieval, and theory of computation. Cognitive Computing has connections and is informed by research in behavioral sciences, cognitive neuroscience, data science, big data, algorithms, programming languages, information systems, and design. While Cognitive Computing deals with theoretical questions surrounding ability of machines to learn and exhibit intelligence, it is fundamentally an interdisciplinary area that is driven by a wide range of real life challenges at the interface between humans and machines.</p>
+				<h2>What is Hybrid Intelligence?</h2>
+				<p>Hybrid Intelligence is an umbrella term that refers to creating software systems that exhibit ability to learn, adapt, understand context, and work seamlessly with humans and for humans. Core areas that are part of Hybrid Intelligence include artificial intelligence, machine learning, human-computer interaction, cognitive sciences, natural language processing, computer vision, data science, information retrieval, and large databases. Hybrid Intelligence has connections and is informed by research in behavioral sciences, cognitive neuroscience, data science, big data, algorithms, programming languages, information systems, and design. While Hybrid Intelligence deals with theoretical questions surrounding ability of machines to learn and exhibit intelligence, it is fundamentally an interdisciplinary area that is driven by a wide range of real life challenges at the interface between humans and machines.</p>
 			</div>
 		</div>
