c7203e0fc9cc9b9380b85f789a23c66694507003
Ashis Kumar Chanda <ashis415@yahoo.com>
2021-08-26 15:34:52 -0400

    Update people.php

--- application/views/people/people.php
+++ application/views/people/people.php
@@ -1,103 +1,103 @@
 
-		<div class="row">
-			<div class="col-sm-12">
+    <div class="row">
+      <div class="col-sm-12">
         <div class="site-header">
-				  <h1>People</h1>
+          <h1>People</h1>
         </div>
-				<p>Center for Cognitive Computer (<abbr title="Center for Cognitive Computing">CCC</abbr>) faculty have expertise in Artificial Intelligence, Machine Learning, Data Science, Computer Vision, Robotics, Natural Language Processing, Information Retrieval, Collaborative Systems, Theory of Computation, Behavioral Science, Cognitive Neuroscience, Optimization, Applied Math, and Software Development. CCC will continue to grow through hiring of new core faculty members, increasing its affiliate faculty membership, involving students in cognitive computing research, and developing collaborations with regional research institutions, industry, and nonprofits.</p>
-			</div>
-		</div>
+        <p>Center for Hybrid Intelligence (<abbr title="Center for Hybrid Intelligence">CHI</abbr>) faculty have expertise in Artificial Intelligence, Machine Learning, Human-Computer Interaction, Collaborative Systems, Data Science, Computer Vision, Robotics, Natural Language Processing, Information Retrieval, Behavioral Science, Cognitive Neuroscience, Optimization, Applied Math, and Software Development. CHI will continue to grow through hiring of new core faculty members, increasing its affiliate faculty membership, involving students in cognitive computing research, and developing collaborations with regional research institutions, industry, and nonprofits.</p>
+      </div>
+    </div>
 
     <!-- CORE FACULTY -->
     <!-- photo/name/department/specialty/email -->
     <div class="people-section row">
-			<div class="col-sm-12">
-				<h2>Core Faculty</h2>
+      <div class="col-sm-12">
+        <h2>Core Faculty</h2>
         <em class="d-md-none">(click on profile for more info)</em>
-			</div>
+      </div>
       
       <!-- Person -->
-			<div class="col-sm-4">
+      <div class="col-sm-4">
         <a href="vucetic" title="See more information">
           <img src="images/people/core_faculty/vucetic.png" alt="Slobodan Vucetic" />
         </a>
-				<h4 class="name">Slobodan Vucetic, Director</h4>
+        <h4 class="name">Slobodan Vucetic, Director</h4>
           <div class="department">Computer and Information Sciences</div>
-          <div class="specialty">(machine learning, data science)</div>
+          <div class="specialty">(artificial intelligence, data science)</div>
           <a href="mailto:vucetic@temple.edu">e-mail</a> / 
           <a href="http://www.dabi.temple.edu/~vucetic/" target="_blank">website</a> /
           <a href="vucetic">more info</a>
-			</div>
+      </div>
       <!-- End Person -->
       
       <!-- Person -->
-			<div class="col-sm-4">
+      <div class="col-sm-4">
         <a href="ling" title="See more information">
-          <img src="images/people/core_faculty/ling.png" alt="Haibin Ling" />
+          <img src="images/people/core_faculty/no-image.svg" alt="Stephen MacNeil" />
         </a>
-				<h4 class="name">Haibin Ling</h4>
+        <h4 class="name">Stephen MacNeil</h4>
           <div class="department">Computer and Information Sciences</div>
-          <div class="specialty">(computer vision, robotics)</div>
-          <a href="mailto:haibin.ling@gmail.com">e-mail</a> / 
-          <a href="http://www.dabi.temple.edu/~hbling/" target="_blank">website</a> /
+          <div class="specialty">(human-computer interaction, cooperative systems)</div>
+          <a href="mailto:stephen.macneil@gmail.com">e-mail</a> / 
+          <a href="https://stevemacn.github.io/" target="_blank">website</a> /
           <a href="ling">more info</a>
-			</div>
+      </div>
       <!-- End Person -->
       
       <!-- Person -->
-			<div class="col-sm-4">
+      <div class="col-sm-4">
         <a href="wang" title="See more information">
           <img src="images/people/core_faculty/no-image.svg" alt="Placeholder silhouette image of person" />
         </a>
-				<h4 class="name">Pei Wang</h4>
+        <h4 class="name">Pei Wang</h4>
           <div class="department">Computer and Information Sciences</div>
           <div class="specialty">(artificial intelligence)</div>
           <a href="mailto:pei.wang@temple.edu">e-mail</a> / 
           <a href="https://cis.temple.edu/~pwang/" target="_blank">website</a> /
           <a href="wang">more info</a>
-			</div>
+      </div>
       <!-- End Person -->
       
       <!-- Person -->
-			<div class="col-sm-4">
+      <div class="col-sm-4">
         <a href="dragut" title="See more information">
           <img src="images/people/core_faculty/dragut.png" alt="Eduard Dragut" />
         </a>
-				<h4 class="name">Eduard Dragut</h4>
+        <h4 class="name">Eduard Dragut</h4>
           <div class="department">Computer and Information Sciences</div>
           <div class="specialty">(information integration, natural language processing)</div>
           <a href="mailto:edragut@temple.edu">e-mail</a> / 
           <a href="https://cis.temple.edu/~edragut/" target="_blank">website</a> /
           <a href="dragut">more info</a>
-			</div>
+      </div>
       <!-- End Person -->
       
       <!-- Person -->
-			<div class="col-sm-4">
+      <div class="col-sm-4">
         <a href="ji" title="See more information">
-          <img src="images/people/core_faculty/ji.png" alt="Bo Ji" />
+          <img src="images/people/core_faculty/no-image.svg" alt="Hongchang Gao" />
         </a>
-				<h4 class="name">Bo Ji</h4>
+        <h4 class="name">Hongchang Gao</h4>
+          <div class="department">Computer and Information Sciences</div>
+          <div class="specialty">(machine learning)</div>
+          <a href="mailto:hongchang.gao@temple.edu">e-mail</a> / 
+          <a href="https://www.cst.temple.edu/~tuo14379/" target="_blank">website</a> /
+          <a href="ji">more info</a>
+      </div>
+      <!-- End Person -->
+      
+      <!-- Person -->
+      <div class="col-sm-4">
+        <a href="wehar" title="See more information">
+          <img src="images/people/core_faculty/no-image.svg" alt="TBD" />
+        </a>
+        <h4 class="name">TBD</h4>
           <div class="department">Computer and Information Sciences</div>
           <div class="specialty">(artificial intelligence)</div>
-          <a href="mailto:boji@temple.edu">e-mail</a> / 
-          <a href="https://cis.temple.edu/~boji/" target="_blank">website</a> /
-          <a href="ji">more info</a>
-			</div>
-      <!-- End Person -->
-      
-      <!-- Person -->
-			<div class="col-sm-4">
-        <a href="wehar" title="See more information">
-          <img src="images/people/core_faculty/wehar.png" alt="Michael Wehar" />
-        </a>
-				<h4 class="name">Michael Wehar</h4>
-          <div class="department">Computer and Information Sciences</div>
-          <div class="specialty">(information integration, natural language processing)</div>
-          <a href="mailto:michael.wehar@temple.edu">e-mail</a> / 
-          <a href="http://michaelwehar.com/" target="_blank">website</a> /
+          <a href="mailto:***@temple.edu">e-mail</a> / 
+          <a href="http:// " target="_blank">website</a> /
           <a href="wehar">more info</a>
-			</div>
+      </div>
       <!-- End Person -->
       
     </div>
@@ -107,61 +107,61 @@
     <!-- AFFILIATED FACULTY -->
     <!-- image/name/department/specialty -->
     <div class="people-section row">
-			<div class="col-sm-12">
-				<h2>Afilliated Faculty</h2>
-			</div>
+      <div class="col-sm-12">
+        <h2>Afilliated Faculty</h2>
+      </div>
       
       <!-- Person -->
-			<div class="col-sm-3">
+      <div class="col-sm-3">
         <img src="images/people/core_faculty/no-image.svg" alt="Placeholder silhouette image of person" />
-				<h4>Donald Hantula</h4>
+        <h4>Donald Hantula</h4>
           <div class="department">Psychology</div>
           <div class="specialty">(behavior science)</div>
-			</div>
+      </div>
       
       <!-- Person -->
-			<div class="col-sm-3">
+      <div class="col-sm-3">
         <img src="images/people/core_faculty/no-image.svg" alt="Placeholder silhouette image of person" />
-				<h4>Kevin Henry</h4>
+        <h4>Kevin Henry</h4>
           <div class="department">Geography</div>
           <div class="specialty">(spatial epidemiology)</div>
-			</div>
+      </div>
       <!-- End Person -->
       
       <!-- Person -->
-			<div class="col-sm-3">
+      <div class="col-sm-3">
         <img src="images/people/core_faculty/no-image.svg" alt="Placeholder silhouette image of person" />
-				<h4>Daniel Jacobs</h4>
+        <h4>Daniel Jacobs</h4>
           <div class="department">Mechanical Engineering</div>
           <div class="specialty">(assistive robotics)</div>
-			</div>
+      </div>
       <!-- End Person -->
       
       <!-- Person -->
-			<div class="col-sm-3">
+      <div class="col-sm-3">
         <img src="images/people/core_faculty/no-image.svg" alt="Placeholder silhouette image of person" />
-				<h4>John Nosek</h4>
+        <h4>John Nosek</h4>
           <div class="department">Computer and Information Sciences</div>
           <div class="specialty">(collaborative learning)</div>
-			</div>
+      </div>
       <!-- End Person -->
       
       <!-- Person -->
-			<div class="col-sm-3">
+      <div class="col-sm-3">
         <img src="images/people/core_faculty/no-image.svg" alt="Placeholder silhouette image of person" />
-				<h4>Benjamin Seibold</h4>
+        <h4>Benjamin Seibold</h4>
           <div class="department">Mathematics</div>
           <div class="specialty">(applied math)</div>
-			</div>
+      </div>
       <!-- End Person -->
       
       <!-- Person -->
-			<div class="col-sm-3">
+      <div class="col-sm-3">
         <img src="images/people/core_faculty/no-image.svg" alt="Placeholder silhouette image of person" />
-				<h4>Geoffrey Wright</h4>
+        <h4>Geoffrey Wright</h4>
           <div class="department">Public Health</div>
           <div class="specialty">(cognitive neuroscience)</div>
-			</div>
+      </div>
       <!-- End Person -->
       
     </div>
