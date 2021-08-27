bb5afa4625c0602d2af6c22126c1aa31e286ada8
Ashis Kumar Chanda <ashis415@yahoo.com>
2021-08-27 12:56:11 -0400

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -5,7 +5,7 @@
 				  <h1>Our Mission</h1>
         </div>
 				<p> The term hybrid intelligence refers to approaches and technology that enable cooperation between humans and intelligent machines. Temple University’s Center for Hybrid Intelligence (<abbr title="Center for Hybrid Intelligence">CHI</abbr>) is committed to
-				promoting research, education, and tech transfer activities towards
+				promoting research, education, and software development activities towards
 				building computer systems that could learn from experience, adapt, exhibit intelligent
 				behavior, cooperate with humans, and improve human condition. </p>
 				<p>We aim to become a center of excellence in artificial intelligence and cognitive computing and a catalyst
@@ -24,28 +24,28 @@
         <div class="news news-container">
           
           <!--News Entry-->
-          <div class="date">June 30, 2021</div>
-          <p>We are welcoming our new CHI member Dr. Stephen MacNeil (http://stevemacn.github.io/), who is starting his tenure track Assistant Professor appointment in the Department of Computer and Information Sciences in Fall 2021. Stephen is joining us from the UCSD Design Lab, where he was a postdoctoral researcher. In his research, Stephen is building on theories from learning sciences, social computing, and human-computer interaction to democratize design and education by integrating the perspectives of non-experts. </p>
+          <div class="date">August 15, 2021</div>
+          <p>We are welcoming our new CHI member <a href="http://stevemacn.github.io/"> Dr. Stephen MacNeil </a>, who is starting his tenure track Assistant Professor appointment in the Department of Computer and Information Sciences in Fall 2021. Stephen is joining us from the UCSD Design Lab, where he was a postdoctoral researcher. In his research, Stephen is building on theories from learning sciences, social computing, and human-computer interaction to democratize design and education by integrating the perspectives of non-experts. </p>
           <!--End Entry-->
 
           <!--News Entry-->
-          <div class="date">January 30, 2019</div>
+          <div class="date">August 1, 2021</div>
           <p>Slobodan Vucetic and Jie Wu from Temple received a new collaborative NSF grant with their collaborators Shiwen Mao from Auburn and Xuyu Wang from California State at Sacramento for project “Data Augmentation and Adaptive Learning for Next Generation Wireless Spectrum Systems.” The primary goal of this project is to devise innovative approaches that enable wireless researchers and practitioners to acquire data more efficiently at reduced cost and to utilize existing data more effectively. </p>
           <!--End Entry-->
           
           <!--News Entry-->
           <div class="date">April 15, 2021</div>
-          <p>CHI professors Eduard Dragut and Slobodan Vucetic are co-organizing with Lucian Popa and Yunyao Li from IBM Research Almaden the 3rd Workshop on Data Science with Human in the Loop @ KDD 2021 (https://www.dashworkshops.org/kdd2021). The goal of this workshop is to stimulate research on human-computer cooperation challenges in data science. The workshop will focus on understanding how to leverage respective strengths of humans and computers along the data science pipeline and in a wide range of data science tasks and real-life applications.  </p>
+          <p>CHI professors Eduard Dragut and Slobodan Vucetic are co-organizing with Lucian Popa and Yunyao Li from IBM Research Almaden the <a href="https://www.dashworkshops.org/kdd2021"> 3rd Workshop on Data Science with Human in the Loop @ KDD 2021</a>. The goal of this workshop is to stimulate research on human-computer cooperation challenges in data science. The workshop will focus on understanding how to leverage respective strengths of humans and computers along the data science pipeline and in a wide range of data science tasks and real-life applications.  </p>
           <!--End Entry-->
           
           <!--News Entry-->
           <div class="date">January 15, 2021</div>
-          <p>CHI professors Eduard Dragut and Slobodan Vucetic are co-organizing with Lucian Popa and Yunyao Li from IBM Research Almaden the 2rd Workshop on Data Science with Human in the Loop @ NAACL 2021 (https://sites.google.com/view/dash-la2021/home). The goal of this workshop is to stimulate research on understanding how to optimize human-computer cooperation and minimize human effort along the NLP pipeline in a wide range of NLP tasks and real-world applications.  </p>
+          <p>CHI professors Eduard Dragut and Slobodan Vucetic are co-organizing with Lucian Popa and Yunyao Li from IBM Research Almaden the <a href="https://sites.google.com/view/dash-la2021/home"> 2rd Workshop on Data Science with Human in the Loop @ NAACL 2021</a>. The goal of this workshop is to stimulate research on understanding how to optimize human-computer cooperation and minimize human effort along the NLP pipeline in a wide range of NLP tasks and real-world applications.  </p>
           <!--End Entry-->
 
           <!--News Entry-->
           <div class="date">September 30, 2020</div>
-          <p> Slobodan Vucetic, Eduard Dragut, Donald Hantula, and Matt Tincani from Temple are teaming up with Sungsoo Hong from George Mason on a new NSF grant “Personalized Virtual Job Assistants to Prepare Individuals with Neurodevelopmental Disabilities for Entry Level IT Jobs” (https://www.nsf.gov/awardsearch/showAward?AWD_ID=2026513&HistoricalAwards=false). This project brings together an interdisciplinary team of researchers with expertise in data science, software engineering, human-computer interaction, behavioral science, and special education, who will collaborate with vocational programs and employers to transform the current state of employment for young adults with mild to moderate neurodevelopmental disabilities. </p>
+          <p> Slobodan Vucetic, Eduard Dragut, Donald Hantula, and Matt Tincani from Temple are teaming up with Sungsoo Hong from George Mason on a new NSF grant “<a href='https://www.nsf.gov/awardsearch/showAward?AWD_ID=2026513&HistoricalAwards=false'>Personalized Virtual Job Assistants to Prepare Individuals with Neurodevelopmental Disabilities for Entry Level IT Jobs'</a>. This project brings together an interdisciplinary team of researchers with expertise in data science, software engineering, human-computer interaction, behavioral science, and special education, who will collaborate with vocational programs and employers to transform the current state of employment for young adults with mild to moderate neurodevelopmental disabilities. </p>
           <!--End Entry-->
           
         </div><!-- End news-container -->
@@ -69,7 +69,7 @@
 				<div class="home-card card">
 				  <h5 class="home-card-title card-title">Our Research</h5>
 					<div class="home-card-body card-body">
-						<p class="card-text">Our faculty and students are involved in a range of projects addressing fundamental questions in artificial intelligence, human-computer interaction, and data science, as well as in applied interdisciplinary research.</p>
+						<p class="card-text">We are involved in a range of projects addressing fundamental questions in artificial intelligence, human-computer interaction, and data science, as well as in applied interdisciplinary research.</p>
 						<a class="home-card-btn btn btn-primary" href="http://chi.temple.edu/research">Learn More</a>
 					</div>
 				</div>
@@ -102,14 +102,14 @@
 			</div>
 			<div class="col-md-8">
 				<h2>If you need our expertise</h2>
-				<p>Our core and affiliated faculty are doing cutting edge research and development and have broad and diverse expertise in areas related to hybrid intelligence. CHI provides its expertise to individual and organizations at Temple University, Greater Philadelphia, nationally, and internationally. It aims to engage in research and development projects with academia, government, and industry partners. We will be happy to talk to you and see how we can help! Please do not hesitate to contact us. 
+				<p>Our core and affiliated faculty are doing cutting edge research and development and have broad and diverse expertise in areas related to hybrid intelligence. CHI provides its expertise to individual and organizations at Temple University, Greater Philadelphia, nationally, and internationally. It aims to engage in research and development projects with academia, government, and industry partners. We will be happy to talk to you and see how we can help! Please do not hesitate to <a href="http://chi.temple.edu/contact"> contact us</a>. 
 </p>
 			</div>
 		</div>
 		<div class="home-section row flex-column-reverse flex-md-row">
 			<div class="col-md-8">
 				<h2>If you are interested in corporate partnership</h2>
-				<p>CHI aims to develop mutually beneficial corporate partnerships and engage in short term and long term collaboration with industry partners. CHI provides access to its human and computational resources and to its talented undergraduate and graduate students. We provide a range of research and development services ranging from consulting to research projects to software development. We translate our research into new products and services. We are developing customized courses and programs for professional training and continued education. </p>
+				<p>CHI aims to develop mutually beneficial corporate partnerships and engage in short term and long term collaborations. CHI provides access to its human and computational resources and to its talented undergraduate and graduate students. We provide a range of services ranging from consulting to research projects to software development. We translate our research into new products and services. We are developing customized courses and programs for professional training and continued education. </p>
 			</div>
 			<div class="col-md-4">
 				<img src="images/corporate_partnership.svg" alt="Graphic on corporate partnership" />
